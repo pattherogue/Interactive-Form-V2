777ffbaee2d776a7612041f1077ccb159147b926
Paolo Gomez <78209142+pattherogue@users.noreply.github.com>
2021-08-08 11:14:11 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -153,7 +153,7 @@ const validateName = () => {
 const validateEmail = () => {
     const enteredEmail = inputEmail.value;
     /* username, "@" symbol, ".com" domain name */
-    const emailValidated = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(enteredEmail.value);
+    const emailValidated = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(enteredEmail);
     return emailValidated;
 };
 
